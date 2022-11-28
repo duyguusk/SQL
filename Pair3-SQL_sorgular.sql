1c3f09d6b24adbc6e72bde5928e5c70656199ac9
Duygu Selin Kara <97129378+duyguusk@users.noreply.github.com>
2022-11-28 15:36:03 +0300

    Add files via upload

---
+++ Pair3-SQL_sorgular.sql
@@ -0,0 +1,75 @@
+Select * from products
+
+Select * from products
+order by name desc
+
+Select * from products
+order by name asc
+
+Select * from products
+order by stock 
+
+Select * from products
+order by stock desc
+
+Select * from products
+Where stock = 10
+
+Select * from products
+Where stock <= 15
+
+Select * from products
+Where unit_prices = '£800'
+
+Select * from products
+order by name desc
+
+Select * from products
+order by name asc
+
+Select * from products
+order by stock 
+
+Select * from products
+order by stock desc
+
+Select * from products
+Where stock = 10
+
+Select * from products
+Where stock <= 15
+
+Select * from products
+Where unit_prices = '£800'
+
+Select * from products
+Where stock > 10 and unit_prices > '£900'
+
+Select * from products
+Where stock < 12 or unit_prices > '£800'
+
+Select * from products
+Where stock >= 15 or unit_prices >= '£800'
+
+Select * from products
+Where name = 'kulaklık' or name = 'telefon'
+
+Select * from products
+Where name IN ('Masaüstü', 'telefon', 'Laptop')
+
+Select * from products
+Where stock in (30, 8, 15)
+
+Select * from products
+Where stock between 10 and 20
+
+Select * from products
+Where name like '___a%'
+
+Select * from products
+Where lower(name) like '%a%'
+
+Select * from products
+Where lower(name) like 't%'
+
+
